ab2124fac279c64f32c147942861b7c7ab57f659
Gopal Krishna Kini <gopal@caliberassurance.com>
2018-01-31 18:01:31 +0530

    Added pom.xml from start.spring.io

---
+++ src/main/java/com/strugglingcoder/springjpaexplorer/SpringJpaExplorerApplication.java
@@ -0,0 +1,12 @@
+package com.strugglingcoder.springjpaexplorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaExplorerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaExplorerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/strugglingcoder/springjpaexplorer/SpringJpaExplorerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.strugglingcoder.springjpaexplorer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringJpaExplorerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
